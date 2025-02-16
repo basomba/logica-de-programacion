e763301c359f5adda035a64494f7b44c850cfb67
basomba <150730890+basomba@users.noreply.github.com>
2025-02-16 15:08:44 -0300

    Add files via upload

---
+++ 1 java logica de programacion - variables/1 variables/variablesstring/src/variablesstring/Variablesstring.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variablesstring;
+
+/**
+ *
+ * @author indianmachinery club
+ */
+public class Variablesstring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String saludo = "hola mundo";
+        System.out.println(saludo);
+        // TODO code application logic here
+    }
+    
+}
